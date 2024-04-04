9d9c6555c42d12a0c7c28e73842a139d29a75224
Priyadi Iman Nurcahyo <1102197+priyadi@users.noreply.github.com>
2024-04-04 14:13:41 +0700

    fix: js module

--- packages/rekapager-bundle/assets/dist/infinite-scrolling.js
+++ packages/rekapager-bundle/assets/dist/infinite-scrolling.js
@@ -9,11 +9,6 @@
 
 'use strict';
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports["default"] = void 0;
-var _stimulus = require("@hotwired/stimulus");
 function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
 function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread(); }
 function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
@@ -34,7 +29,8 @@ function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf ? Objec
 function _defineProperty(obj, key, value) { key = _toPropertyKey(key); if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 function _toPropertyKey(t) { var i = _toPrimitive(t, "string"); return "symbol" == _typeof(i) ? i : i + ""; }
 function _toPrimitive(t, r) { if ("object" != _typeof(t) || !t) return t; var e = t[Symbol.toPrimitive]; if (void 0 !== e) { var i = e.call(t, r || "default"); if ("object" != _typeof(i)) return i; throw new TypeError("@@toPrimitive must return a primitive value."); } return ("string" === r ? String : Number)(t); }
-var _default = exports["default"] = /*#__PURE__*/function (_Controller) {
+import { Controller } from '@hotwired/stimulus';
+var _default = /*#__PURE__*/function (_Controller) {
   function _default() {
     _classCallCheck(this, _default);
     return _callSuper(this, _default, arguments);
@@ -114,7 +110,7 @@ var _default = exports["default"] = /*#__PURE__*/function (_Controller) {
       });
     }
   }]);
-}(_stimulus.Controller);
+}(Controller);
 _defineProperty(_default, "values", {
   breakpoint: {
     type: String,
@@ -125,3 +121,4 @@ _defineProperty(_default, "values", {
     "default": '.pagination'
   }
 });
+export { _default as default };
