f8a26eb9be4c8fe50e253e3510c36ccedc05f0d5
Priyadi Iman Nurcahyo <1102197+priyadi@users.noreply.github.com>
2024-05-20 18:56:56 +0700

    fix: add void return in bundle class

--- packages/rekapager-bundle/src/RekalogikaRekapagerBundle.php
+++ packages/rekapager-bundle/src/RekalogikaRekapagerBundle.php
@@ -24,7 +24,7 @@ class RekalogikaRekapagerBundle extends Bundle
         return \dirname(__DIR__);
     }
 
-    public function build(ContainerBuilder $container)
+    public function build(ContainerBuilder $container): void
     {
         parent::build($container);
 
