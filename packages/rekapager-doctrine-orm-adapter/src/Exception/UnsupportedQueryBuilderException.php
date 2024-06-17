14e65e61e8650efa426f4ed3af4c007112ee1ec2
Priyadi Iman Nurcahyo <1102197+priyadi@users.noreply.github.com>
2024-06-17 11:04:30 +0700

    feat(`QueryBuilderAdapter`): refuse to continue if the supplied criteria has a first result or max results parameter set (#81)

---
+++ packages/rekapager-doctrine-orm-adapter/src/Exception/UnsupportedQueryBuilderException.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of rekalogika/rekapager package.
+ *
+ * (c) Priyadi Iman Nurcahyo <https://rekalogika.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
+ */
+
+namespace Rekalogika\Rekapager\Doctrine\ORM\Exception;
+
+use Rekalogika\Contracts\Rekapager\Exception\UnexpectedValueException;
+
+class UnsupportedQueryBuilderException extends UnexpectedValueException
+{
+    public function __construct()
+    {
+        parent::__construct('Unsupported QueryBuilder. The supplied QueryBuilder must not have a "first result" or "max results" parameters set in the constructor or by calling "setFirstResult()" or "setMaxResults()".');
+    }
+}
