10f3491d7691952ed3f7417e140b825876ba333b
Priyadi Iman Nurcahyo <1102197+priyadi@users.noreply.github.com>
2024-06-20 18:31:05 +0700

    docs: add comments for documentation (#95)

--- packages/rekapager-contracts/src/PageInterface.php
+++ packages/rekapager-contracts/src/PageInterface.php
@@ -18,6 +18,8 @@ namespace Rekalogika\Contracts\Rekapager;
  * throws OutOfBoundsException if it is empty, except for the first page, where
  * it must return an empty list.
  *
+ * @see https://rekalogika.dev/rekapager/pageable-page
+ *
  * @template TKey of array-key
  * @template-covariant T
  * @extends \Traversable<TKey,T>
@@ -25,36 +27,50 @@ namespace Rekalogika\Contracts\Rekapager;
 interface PageInterface extends \Traversable, \Countable
 {
     /**
-     * Gets the page identifier
+     * Gets the page identifier. Can be used with
+     * PageableInterface::getPageByIdentifier to get the same page again.
      */
     public function getPageIdentifier(): object;
 
     /**
      * Gets the page number. This is not used for calculation, only for
-     * showing the page number to the user. Doesn't have to be accurate.
-     * Null means the page number is unknown. Negative numbers mean the page
-     * is counted from the end of the result set.
+     * presentation. Doesn't have to be accurate. Null means the page number is
+     * unknown. Negative numbers mean the page is counted from the end of the
+     * result set.
      */
     public function getPageNumber(): ?int;
 
+    /**
+     * Used by the framework to renumber pages if it is known by the pager that
+     * the page number is inaccurate,
+     */
     public function withPageNumber(?int $pageNumber): static;
 
     /**
+     * Gets the pageable object that contains this page.
+     *
      * @return PageableInterface<TKey,T>
      */
     public function getPageable(): PageableInterface;
 
     /**
+     * Gets the number of items per page. The actual items in the page may be
+     * less than this number if the page is the last page.
+     *
      * @return int<1,max>
      */
     public function getItemsPerPage(): int;
 
     /**
+     * Gets the next page if it exists.
+     *
      * @return null|PageInterface<TKey,T>
      */
     public function getNextPage(): ?PageInterface;
 
     /**
+     * Gets the previous page if it exists.
+     *
      * @return null|PageInterface<TKey,T>
      */
     public function getPreviousPage(): ?PageInterface;
