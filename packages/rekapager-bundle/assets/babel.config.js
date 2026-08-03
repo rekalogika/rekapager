3bd60ab135aa1932f322491adf079a5532fbfab8
Priyadi Iman Nurcahyo <1102197+priyadi@users.noreply.github.com>
2026-08-03 14:52:35 +0700

    deps(js): update to babel 8

--- packages/rekapager-bundle/assets/babel.config.js
+++ packages/rekapager-bundle/assets/babel.config.js
@@ -1,11 +1,31 @@
 module.exports = {
     presets: [
         ['@babel/preset-env', {
-            "loose": true,
             "modules": false
         }]
     ],
+    // Babel 8 removed preset-env's "loose" option. These are the granular
+    // assumptions that "loose": true used to enable, except
+    // superIsCallableConstructor, which we keep disabled.
     assumptions: {
+        arrayLikeIsIterable: true,
+        constantReexports: true,
+        enumerableModuleMeta: true,
+        ignoreFunctionLength: true,
+        ignoreToPrimitiveHint: true,
+        iterableIsArray: true,
+        mutableTemplateObject: true,
+        noClassCalls: true,
+        noDocumentAll: true,
+        noIncompleteNsImportDetection: true,
+        objectRestNoSymbols: true,
+        privateFieldsAsProperties: true,
+        pureGetters: true,
+        setClassMethods: true,
+        setComputedProperties: true,
+        setPublicClassFields: true,
+        setSpreadProperties: true,
+        skipForOfIteratorClosing: true,
         superIsCallableConstructor: false,
     },
 };
